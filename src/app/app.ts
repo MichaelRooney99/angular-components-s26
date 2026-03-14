3889dcb18b2d5903c796fc7c59cbd24ec27a9b0b
Michael Rooney <msroone@gmail.com>
2026-03-14 14:59:37 -0500

    app root addition

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
 import { TsteeleAddition } from './tsteele-addition/tsteele-addition';
+import { MrooneyAddition } from './mrooney-addition/mrooney-addition';
 
 @Component({
   selector: 'app-root',
   imports: [
     PizzaToppings,
     TsteeleAddition,
+    MrooneyAddition
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
